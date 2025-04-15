dfeb17759b88e561402df4871309448049622d2b
Briar <briar.j.mills@gmail.com>
2025-04-15 11:13:04 -0400

    HRP/GHO status (#1)
    * Check HRP/GHO status
    
    * Remove test logging
    
    * Add location
    
    * Update schedule

--- src/hdx/scraper/hrp_projects/__main__.py
+++ src/hdx/scraper/hrp_projects/__main__.py
@@ -10,6 +10,7 @@ from copy import deepcopy
 from os.path import dirname, expanduser, join
 
 from hdx.api.configuration import Configuration
+from hdx.api.utilities.hdx_error_handler import HDXErrorHandler
 from hdx.data.user import User
 from hdx.facades.infer_arguments import facade
 from hdx.utilities.dateparse import now_utc
@@ -47,52 +48,59 @@ def main(
         raise PermissionError(
             "API Token does not give access to HPC Tools organisation!"
         )
-    with State(
-        "update_dates.txt",
-        State.dates_str_to_country_date_dict,
-        State.country_date_dict_to_dates_str,
-    ) as state:
-        state_dict = deepcopy(state.get())
-        with temp_dir_batch(folder=_USER_AGENT_LOOKUP) as info:
-            temp_dir = info["folder"]
-            with Download() as downloader:
-                retriever = Retrieve(
-                    downloader=downloader,
-                    fallback_dir=temp_dir,
-                    saved_dir=_SAVED_DATA_DIR,
-                    temp_dir=temp_dir,
-                    save=save,
-                    use_saved=use_saved,
-                )
-                hrp_projects = HRPProjects(configuration, retriever, temp_dir)
-                now = now_utc()
-                cutoff_year = now.year - 5
-                hrp_projects.get_data(cutoff_year)
-                countryiso3s = hrp_projects.check_state(state_dict)
-                for countryiso3 in countryiso3s:
-                    dataset = hrp_projects.generate_dataset(countryiso3)
-                    if not dataset:
-                        continue
-                    dataset.update_from_yaml(
-                        path=join(
-                            dirname(__file__), "config", "hdx_dataset_static.yaml"
+    with HDXErrorHandler(should_exit_on_error=True) as error_handler:
+        with State(
+            "update_dates.txt",
+            State.dates_str_to_country_date_dict,
+            State.country_date_dict_to_dates_str,
+        ) as state:
+            state_dict = deepcopy(state.get())
+            with temp_dir_batch(folder=_USER_AGENT_LOOKUP) as info:
+                temp_dir = info["folder"]
+                with Download() as downloader:
+                    retriever = Retrieve(
+                        downloader=downloader,
+                        fallback_dir=temp_dir,
+                        saved_dir=_SAVED_DATA_DIR,
+                        temp_dir=temp_dir,
+                        save=save,
+                        use_saved=use_saved,
+                    )
+                    hrp_projects = HRPProjects(
+                        configuration, retriever, error_handler, temp_dir
+                    )
+                    now = now_utc()
+                    hrp_projects.get_data(
+                        current_year=now.year, cutoff_year=now.year - 5
+                    )
+                    hrp_projects.check_hrp_gho()
+                    countryiso3s = hrp_projects.check_state(state_dict)
+                    for countryiso3 in countryiso3s:
+                        dataset = hrp_projects.generate_dataset(countryiso3)
+                        if not dataset:
+                            continue
+                        dataset.update_from_yaml(
+                            path=join(
+                                dirname(__file__), "config", "hdx_dataset_static.yaml"
+                            )
                         )
-                    )
-                    dataset.generate_quickcharts(
-                        resource=0,
-                        path=join(
-                            dirname(__file__), "config", "hdx_resource_view_static.yaml"
-                        ),
-                    )
-                    dataset.create_in_hdx(
-                        remove_additional_resources=True,
-                        match_resource_order=False,
-                        hxl_update=False,
-                        updated_by_script=_UPDATED_BY_SCRIPT,
-                        batch=info["batch"],
-                    )
+                        dataset.generate_quickcharts(
+                            resource=0,
+                            path=join(
+                                dirname(__file__),
+                                "config",
+                                "hdx_resource_view_static.yaml",
+                            ),
+                        )
+                        dataset.create_in_hdx(
+                            remove_additional_resources=True,
+                            match_resource_order=False,
+                            hxl_update=False,
+                            updated_by_script=_UPDATED_BY_SCRIPT,
+                            batch=info["batch"],
+                        )
 
-                state.set(state_dict)
+                    state.set(state_dict)
 
 
 if __name__ == "__main__":
